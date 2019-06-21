f1fe0443816aab921d6dbf40ec34c69b28855bae
dfareri <dominic.fareri@gmail.com>
2019-06-21 09:11:04 -0400

    updates_DF

--- Exclusion/exclAnalysis.R
+++ Exclusion/exclAnalysis.R
@@ -2,11 +2,12 @@
 rm(list=ls());
 
 #set working directory
-setwd('//Users/dfareri/Dropbox/Dominic/Adelphi_Teaching/EmergingScholars_2017_2018/3_2018')
+setwd('//Users/dfareri/Dropbox/Dominic/Github/fareri-lab/WTP_social/Exclusion')
 
 #PrelimWTPdata<-read.table(file = 'PrelimData_updated_3_27_2018_final_20ss.csv', header = TRUE, sep = ',')
-Exclusion_data<-read.table(file = 'Exclusion_AllData2-21-19.csv', header = TRUE, sep = ',')
+Exclusion_data_new<-read.table(file = 'WTP_Exclusion_2-21-19_DF.csv', header = TRUE,sep=',')
 Exclusion_data_wide<-read.table(file = 'Exclusion_Wide_Questionnaires.csv', header = TRUE, sep = ',')
+Exclusion_exc12<-read.table(file = 'exc12.csv', header = TRUE,sep=',')
 
 
 t.test(PrelimWTPdata$chosentype[PrelimWTPdata$Condition==0],mu=.5,na.rm=TRUE)
@@ -19,36 +20,36 @@ t.test(PrelimWTPdata$chosenprice[PrelimWTPdata$Condition==0 & PrelimWTPdata$chos
 t.test(PrelimWTPdata$social.over.non,PrelimWTPdata$nonsocial.over.social,paired = TRUE)
 
 
-subj = c("exc1",
-         "exc2",
-         "exc3",
-         "exc4",
-         "exc5",
-         "exc6",
-         "exc7",
-         "exc8",
-         "exc9",
-         "exc10",
-         "exc11",
-         "exc12",
-         "exc13",
-         "exc14",
-         "exc15",
-         "exc16",
-         "exc17",
-         "exc18",
-         "exc19",
-         "exc20",
-         "exc21",
-         "exc22",
-         "exc23",
-         "exc24",
-         "exc25",
-         "exc26",
-         "exc27",
-         "exc28",
-         "exc29",
-         "exc30");
+subj = c("1",
+         "2",
+         "3",
+         "4",
+         "5",
+         "6",
+         "7",
+         "8",
+         "9",
+         "10",
+         "11",
+         "12",
+         "13",
+         "14",
+         "15",
+         "16",
+         "17",
+         "18",
+         "19",
+         "20",
+         "21",
+         "22",
+         "23",
+         "24",
+         "25",
+         "26",
+         "27",
+         "28",
+         "29",
+         "30");
 
 nsubj=length(subj)
 trial=(1:50)
@@ -58,29 +59,34 @@ PrelimWTPdata_wide<-data.frame(matrix(NA,nrow=20,ncol=16))
 PrelimWTPdata_wide$Subject<-unique(PrelimWTPdata$Subject)
 
 for(i in 1:nsubj){
-  meanSocvsNon<-mean(PrelimWTPdata$social_vs_non[PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
+  #meanSocvsNon<-mean(PrelimWTPdata$social_vs_non[PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
   #print(meanSocvsNon)
-  PrelimWTPdata_wide$SocvsNon[PrelimWTPdata_wide$Subject==subj[i]]<-meanSocvsNon
+  #PrelimWTPdata_wide$SocvsNon[PrelimWTPdata_wide$Subject==subj[i]]<-meanSocvsNon
   
-  meanNonvsSoc<-mean(PrelimWTPdata$nonsocial_vs_social[PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
+  #meanNonvsSoc<-mean(PrelimWTPdata$nonsocial_vs_social[PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
   #print(meanNonvsSoc)
-  PrelimWTPdata_wide$NonvsSoc[PrelimWTPdata_wide$Subject==subj[i]]<-meanNonvsSoc
+  #PrelimWTPdata_wide$NonvsSoc[PrelimWTPdata_wide$Subject==subj[i]]<-meanNonvsSoc
   
-  meanSpentSoc<-mean(PrelimWTPdata$chosenprice[PrelimWTPdata$Condition==0 & PrelimWTPdata$chosentype==1 & PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
+  #meanSpentSoc<-mean(PrelimWTPdata$chosenprice[PrelimWTPdata$Condition==0 & PrelimWTPdata$chosentype==1 & PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
   #print(meanSpentSoc)
-  PrelimWTPdata_wide$AmtSpentSoc[PrelimWTPdata_wide$Subject==subj[i]]<-meanSpentSoc
+  #PrelimWTPdata_wide$AmtSpentSoc[PrelimWTPdata_wide$Subject==subj[i]]<-meanSpentSoc
 
-  meanSpentNonSoc<-mean(PrelimWTPdata$chosenprice[PrelimWTPdata$Condition==0 & PrelimWTPdata$chosentype==0 & PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
-  print(meanSpentNonSoc)
-  PrelimWTPdata_wide$AmtSpentNonSoc[PrelimWTPdata_wide$Subject==subj[i]]<-meanSpentNonSoc
+  #meanSpentNonSoc<-mean(PrelimWTPdata$chosenprice[PrelimWTPdata$Condition==0 & PrelimWTPdata$chosentype==0 & PrelimWTPdata$Subject==subj[i]],na.rm = TRUE)
+  #print(meanSpentNonSoc)
+  #PrelimWTPdata_wide$AmtSpentNonSoc[PrelimWTPdata_wide$Subject==subj[i]]<-meanSpentNonSoc
   
+  AmtChange_PrevPost_SvNS<-mean((Exclusion_data_new$Spre.social..SvNS.[Exclusion_data_new$ChoiceCondition==0 & Exclusion_data_new$Subject==subj[i]])-(Exclusion_data_new$X.Post.social..NvS.[Exclusion_data_new$ChoiceCondition==0 & Exclusion_data_new$Subject==subj[i]]),na.rm=TRUE)
+  print(AmtChange_PrevPost_SvNS)
+  Exclusion_data_wide$AmtChange_PrevPost_SvNS[Exclusion_data_wide$Subject==subj[i]]<-AmtChange_PrevPost_SvNS
 }
 
 for(i in 1:nsubj){
-  Exclusion_data$AQ[Exclusion_data$Subject==subj[i]]<-Exclusion_data_wide$AQ[Exclusion_data_wide$Subject==subj[i]]
-  Exclusion_data$RSE[Exclusion_data$Subject==subj[i]]<-Exclusion_data_wide$RSE[Exclusion_data_wide$Subject==subj[i]]
-}
+  #Exclusion_data$AQ[Exclusion_data$Subject==subj[i]]<-Exclusion_data_wide$AQ[Exclusion_data_wide$Subject==subj[i]]
+  #Exclusion_data_new$RSE[Exclusion_data$Subject==subj[i]]<-Exclusion_data_wide$RSE[Exclusion_data_wide$Subject==subj[i]]
+  Exclusion_data_new$RSE_centered[Exclusion_data_new$Subject==subj[i]]<-(Exclusion_data_new$rse[Exclusion_data_new$Subject==subj[i]] - (mean(Exclusion_data_new$rse,na.rm=TRUE)))
+  }
 
+#mean center self-esteem
 
 
 PrelimWTPdata_SNSOnly<-PrelimWTPdata[PrelimWTPdata$Condition==0,]
---
+++ Exclusion/exclWTP_r15.R
@@ -0,0 +1,15 @@
+rm(list=ls());
+
+
+cor.test(Exclusion_data_wide$AmtChange_PrevPost_SvNS,Exclusion_data_wide$RSE)
+
+Pearson's product-moment correlation
+
+data:  Exclusion_data_wide$AmtChange_PrevPost_SvNS and Exclusion_data_wide$RSE
+t = -1.6865, df = 28, p-value = 0.1028
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.59846885  0.06354656
+sample estimates:
+      cor 
+-0.303675 
